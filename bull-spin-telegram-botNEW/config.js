25302f142b63a760e37062e6f9140674a6639b34
ChinQuan <kamilmobile16@gmail.com>
2025-07-13 20:27:33 +0100

    Delete bull-spin-telegram-botNEW directory

--- bull-spin-telegram-botNEW/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  BULL_MINT: "BuLL65dUKeRgZ1TUo3g9F3SAgJmdwq23mcx7erb9QX9D",
-  SPIN_COST: 1000,
-  DEPOSIT_AMOUNT: 25000,
-  MIN_WITHDRAW: 20000,
-  BACKEND_WALLET: null, // auto-derived from private key
-};
